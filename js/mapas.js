3020438dd87ed9e9192f020d0e743c38c1c76aa1
alanrayado17 <alanrayado17@hotmail.com>
2014-10-07 15:56:11 -0500

    Modificacion a los mapas y a la forma html

--- js/mapas.js
+++ js/mapas.js
@@ -7,41 +7,49 @@ var map;
 function initialize() {
 	//Map options
 	var mapOptions = {
-		zoom : 15
+		zoom : 12,
+		center : new google.maps.LatLng(25.683161, -100.312692),
+		mapTypeId : google.maps.MapTypeId.ROADMAP
 	};
-	
+
 	//Creation of the map object
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-	
+
 	//autocomplete Options
 	var acOptions = {
 		types : ['establishment']
 	};
-	
+
 	//creation of the autocomplete object
 	var ac1 = new google.maps.places.Autocomplete(document.getElementById('ac1'), acOptions);
 	var ac2 = new google.maps.places.Autocomplete(document.getElementById('ac2'), acOptions);
 	//binding of the autocomplete object to the map object
 	ac1.bindTo('bounds', map);
 	ac2.bindTo('bounds', map);
-	//creation of the listener that responds to typed text
-	google.maps.event.addListener(ac1, 'place_changed', function() {
-		infoWindow.close();
-		var place = ac1.getPlace();
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(17);
-		}
-		marker.setPosition(place.geometry.location);
-		infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-		infoWindow.open(map, marker);
-		google.maps.event.addListener(marker, 'click', function(e) {
+	addListener(ac1);
+	addListener(ac2);
+	//creation of the listener that responds to typed tex
+
+	function addListener(ac) {
+		google.maps.event.addListener(ac, 'place_changed', function() {
+			infoWindow.close();
+			var place = ac.getPlace();
+			if (place.geometry.viewport) {
+				map.fitBounds(place.geometry.viewport);
+			} else {
+				map.setCenter(place.geometry.location);
+				map.setZoom(17);
+			}
+			marker.setPosition(place.geometry.location);
+			infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
 			infoWindow.open(map, marker);
+			google.maps.event.addListener(marker, 'click', function(e) {
+				infoWindow.open(map, marker);
+			});
 		});
-	});
-	
+	}
+
+
 	google.maps.event.addListener(ac2, 'place_changed', function() {
 		infoWindow.close();
 		var place = ac2.getPlace();
@@ -59,11 +67,24 @@ function initialize() {
 		});
 	});
 
-	// Try HTML5 geolocation
+}//end of funtion initialize()
+
+function ruta() {
+
+}
+
+function obtenerLocal() {
 	if (navigator.geolocation) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 			var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 			map.setCenter(pos);
+			var marker = new google.maps.Marker({
+				position: pos,
+				map: map,
+				draggable: true,
+				animation: google.maps.Animation.DROP
+			});
+			map.setZoom(17);
 		}, function() {
 			handleNoGeolocation(true);
 		});
@@ -71,7 +92,7 @@ function initialize() {
 		// Browser doesn't support Geolocation
 		handleNoGeolocation(false);
 	}
-}//end of funtion initialize()
+}
 
 //Error handler for Geolocation
 function handleNoGeolocation(errorFlag) {
@@ -89,8 +110,4 @@ function handleNoGeolocation(errorFlag) {
 
 	var infowindow = new google.maps.InfoWindow(options);
 	map.setCenter(options.position);
-}
-
-function ruta(){
-	
 }
